a43dba7fef79e60afef62bd28f6efc2d5000547b
Nemanja0033 <antonijevicnemanja68@gmail.com>
2024-08-23 13:32:48 +0200

    update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { default: axios } = require("axios");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -11,12 +11,17 @@ app.post("/authenticate", async (req, res) => {
 
   try {
     const r = await axios.put('https://api.chatengine.io/users/',
-    {username: username, secret: username, first_name: username},
-    {headers: {"private-key": "1ad00b58-3384-42fb-b33a-6ecc3b0eeec9"}});
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "1ad00b58-3384-42fb-b33a-6ecc3b0eeec9" } }
+    );
     return res.status(r.status).json(r.data);
-  } catch(e){
+  } catch (e) {
     return res.status(e.response.status).json(e.response.data);
   }
 });
 
-app.listen(3001);
+// Use the environment variable PORT provided by Vercel
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
